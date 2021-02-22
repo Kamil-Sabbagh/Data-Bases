0ec176395b7ab86b54815c9ed3db763d7cd5e9b7
Kamil-Sabbagh <69242886+Kamil-Sabbagh@users.noreply.github.com>
2021-02-22 17:53:59 +0300

    Add files via upload

---
+++ week05/kamil sabbagh/create.py
@@ -0,0 +1,17 @@
+import psycopg2
+from faker import Faker
+
+con = psycopg2.connect( database="customers", user="postgres", password="postgres", host="127.0.0.1" )
+print("Database opened successfully")
+cur = con.cursor()
+cur.execute('''CREATE TABLE CUSTOMER
+       (ID INT PRIMARY KEY     NOT NULL,
+       Name           TEXT    NOT NULL,
+       Address            TEXT     NOT NULL,
+       review        TEXT);''')
+print("Table created successfully")
+fake = Faker()
+for i in range(100000):
+
+    cur.execute("INSERT INTO CUSTOMER (ID,Name,Address,review) VALUES ('"+ str(i)+"','"+fake.name()+"','"+fake.address()+"','"+fake.text()+"')")
+    con.commit()
