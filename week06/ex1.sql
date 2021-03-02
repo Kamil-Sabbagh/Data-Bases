94df4b0cfc6c6f5aa9638052b551686eeade03e5
Ahmed Nouralla <40727318+Sh3B0@users.noreply.github.com>
2021-03-02 12:25:08 +0300

    Create ex1.sql

---
+++ week06/ex1.sql
@@ -0,0 +1,19 @@
+-- Non-rented Horror/Sci-Fi films with rating R or PG-13
+
+explain analyze
+select film.film_id, film.title
+from film
+         inner join category on (category.name = 'Horror' or category.name = 'Sci-Fi')
+where (rating = 'R' or rating = 'PG-13')
+
+      -- Exclude rented films
+    except (
+          select i.film_id, f.title
+          from film f,
+               payment p
+                   inner join rental r on p.rental_id = r.rental_id
+                   inner join inventory i on r.inventory_id = i.inventory_id
+      );
+
+-- the most expensive query is the except one, as it uses several joins
+-- adding an index on rental_id will optimize
