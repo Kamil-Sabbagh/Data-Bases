79fa69d97eb8b847fdd4ca8c9e71cf9d17c02b9e
Ahmed Nouralla <40727318+Sh3B0@users.noreply.github.com>
2021-03-09 12:25:16 +0300

    Update function.sql

--- week07/AhmedNouralla/function.sql
+++ week07/AhmedNouralla/function.sql
@@ -1,12 +1,20 @@
+
+drop function if exists get_addr();
 create function get_addr()
-    returns address
+    returns Table
+            (
+                address varchar(50)
+            )
     language plpgsql
 as
 $$
-declare
-    a address;
 begin
-    select * from address as a where a.address like '%' || '11' || '%' and city_id > 400 and city_id < 600;
-    return a;
+    return query
+        select address.address
+        from address
+        where address.address like '%' || '11' || '%' and city_id > 400 and city_id < 600;
 end;
-$$
+$$;
+
+-- test
+select get_addr();
